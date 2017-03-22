4fd455caa9688b77eed25de4d786f887ff1ceac6
sirmdub <sirmdub@gmail.com>
2017-03-22 17:14:52 -0400

    hipchat notification

--- ultrasignupNotifier.py
+++ ultrasignupNotifier.py
@@ -4,6 +4,7 @@ import logging
 import ConfigParser
 import redis
 import re
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
@@ -11,6 +12,7 @@ logger.setLevel(logging.ERROR)
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
 redis_host = config.get("access", "redis_host")
+hipchat_token = config.get("access","hipchat_token")
 r = redis.StrictRedis(host=redis_host)
 defaultsetName = 'ultrasignupNotifier'
 
@@ -70,3 +72,49 @@ def getRedirectURL(url):
 def getPage(url):
     page = requests.get(url)
     return page.text
+
+def hipchat_notify(token, room, message, color='yellow', notify=False,
+                   format='text', host='api.hipchat.com'):
+    """Send notification to a HipChat room via API version 2
+
+    Parameters
+    ----------
+    token : str
+        HipChat API version 2 compatible token (room or user token)
+    room: str
+        Name or API ID of the room to notify
+    message: str
+        Message to send to room
+    color: str, optional
+        Background color for message, defaults to yellow
+        Valid values: yellow, green, red, purple, gray, random
+    notify: bool, optional
+        Whether message should trigger a user notification, defaults to False
+    format: str, optional
+        Format of message, defaults to text
+        Valid values: text, html
+    host: str, optional
+        Host to connect to, defaults to api.hipchat.com
+    """
+
+    if len(message) > 10000:
+        raise ValueError('Message too long')
+    if format not in ['text', 'html']:
+        raise ValueError("Invalid message format '{0}'".format(format))
+    if color not in ['yellow', 'green', 'red', 'purple', 'gray', 'random']:
+        raise ValueError("Invalid color {0}".format(color))
+    if not isinstance(notify, bool):
+        raise TypeError("Notify must be boolean")
+
+    url = "https://{0}/v2/room/{1}/share/link".format(host, room)
+    headers = {'Content-type': 'application/json'}
+    headers['Authorization'] = "Bearer " + token
+    payload = {
+        'message': '@all',
+        'link': message,
+        'notify': notify,
+        'message_format': format,
+        'color': color
+    }
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    r.raise_for_status()
