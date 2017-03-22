3d03b673a89c74c5835d96aea4705c0409417fdd
sirmdub <sirmdub@gmail.com>
2017-03-22 12:03:12 -0400

    actually making the tests verify their work

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,18 @@
 
 from ultrasignupNotifier import *
 
-setURL("http://something", "ultrasignupNotifier_TEST")
-#if not r.sismember("", i):
+setName = "ultrasignupNotifier_TEST"
 
-replaceURL("http://something", "http://somethingelse", "ultrasignupNotifier_TEST")
-delURL("http://somethingelse", "ultrasignupNotifier_TEST")
+setURL("http://something", setName)
+if not r.sismember(setName, "http://something"):
+    raise Exception("FAILED: setURL did not set URL in db")
+
+replaceURL("http://something", "http://somethingelse", setName)
+if r.sismember(setName, "http://something"):
+    raise Exception("FAILED: replaceURL did not remove original URL from db")
+if not r.sismember(setName, "http://somethingelse"):
+    raise Exception("FAILED: replaceURL did not set new URL in db")
+
+delURL("http://somethingelse", setName)
+if r.sismember(setName, "http://somethingelse"):
+    raise Exception("FAILED: delURL did not remove URL from db")
