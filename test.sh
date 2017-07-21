00d2522b014ee3c7d51d91be6c1fa4efb068072d
sirmdub <sirmdub@gmail.com>
2017-07-21 15:27:55 -0400

    SNS notification, with tests, with new aws secrets for tests

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+source tests/aws_secrets.txt
 
 docker run --name some-redis -d redis
-docker run --link some-redis:redis -it ultrasignupnotifier:test
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION --link some-redis:redis -it ultrasignupnotifier:test
 docker stop some-redis
 docker rm some-redis
